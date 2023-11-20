4a85b037705c895b48ff9c986f2444cfafb89ea3
Mani Gidijala <manikhanta0023@gmail.com>
2023-11-20 10:33:16 +0000

    practice

---
+++ models/example/ine_channel_leads.sql
@@ -0,0 +1,1 @@
+SELECT * FROM {{ ref('sales_poc_test_1') }} WHERE first_channel = "INVITE_AND_EARN"
---
+++ models/example/sales_poc_test_1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `dbt-analytics-395911.dbt_mgidijala.sales_poc_test_set`
+
+WHERE first_state = "KERALA"
